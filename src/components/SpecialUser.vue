ffebf10b6b04723a1bc3afceb02bdda46354af61
Your Name <you@example.com>
2022-08-13 15:58:37 -0600

    global event KC

---
+++ src/components/SpecialUser.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <user-list>
+      <h1 ref="clicked" @user_clicked="show_username"></h1>
+    </user-list>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    show_username(user) {
+      this.$refs[`clicked`] = user;
+    },
+  },
+
+  mounted() {
+    this.$root.$on(`user_clicked`, this.show_username);
+  },
+};
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/UserList.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <section v-for="user in users" :key="user">
+      <h1 @click="highlight_username">{{ user }}</h1>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    highlight_username() {
+      this.$root.$emit(`user_clicked`, this.users);
+    },
+  },
+
+  data() {
+    return {
+      users: [`user one`, `user two`, `user three`],
+    };
+  },
+};
+</script>
+
+<style scoped>
+</style>
